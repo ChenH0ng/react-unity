75565d28115d8bdc5557e021d714b877c5606e63
ChenH0ng <chenh0ng@outlook.com>
2017-02-20 22:13:25 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import React, {Component as Base,} from 'react';
 
+const ignore = {
+    constructor: !0,
+    componentDidMount: !0,
+    componentWillMount: !0,
+    componentWillUnmount: !0,
+    componentWillReceiveProps: !0,
+    shouldComponentUpdate: !0,
+    componentWillUpdate: !0,
+    componentDidUpdate: !0,
+};
+
 export const h = React.createElement;
 export class Component extends Base {
+    constructor(props) {
+        super(props);
+        for (let m of Object.getOwnPropertyNames(Object.getPrototypeOf(this))) {
+            if (!ignore[m] && typeof this[m] === 'function') this[m] = this[m].bind(this);
+        }
+        const render = this.render;
+        this.render = () => render(this.props, this.state);
+    }
+
     componentWillMount() {
         this.awake && (this.state = this.awake() || {});
     }
 
     componentDidMount() {
         this.start && this.start();
-    }
-
-    render() {
-        return this.update(this.props, this.state);
     }
 }
